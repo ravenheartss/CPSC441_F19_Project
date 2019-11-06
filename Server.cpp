a982bace94f0523b0a86e4225184ae0aae4a8f34
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2019-11-06 08:58:54 -0700

    fixing issue with user

--- Server.cpp
+++ Server.cpp
@@ -53,7 +53,6 @@ void sendData (int sock, char* buffer, int size);
 
 int main(int argc, char *argv[])
 {
-    inGame = false;
     int serverSock;                  // server socket descriptor
     int clientSock;                  // client socket descriptor
     struct sockaddr_in clientAddr;   // address of the client
